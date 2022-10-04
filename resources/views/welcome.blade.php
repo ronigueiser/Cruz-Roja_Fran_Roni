b25fc1bda934199820614fe58ddfaa808ea9d9ba
ronigueiser <roni.gueiser@davinci.edu.ar>
2022-10-04 17:15:43 -0300

    etiquetas blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,7 @@
           rel="stylesheet"
           integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
           crossorigin="anonymous">
-    <link rel="stylesheet" href="<?= url('css/estilos.css'); ?>">
+    <link rel="stylesheet" href="{{url('css/estilos.css')}}">
 
 </head>
 <body>
@@ -17,20 +17,20 @@
 
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="container-fluid">
-                <a class="navbar-brand" href="<?= url('/') ?>">Cruz Roja</a>
+                <a class="navbar-brand" href="{{url('/')}}">Cruz Roja</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav">
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="<?= url('/'); ?>">Home</a>
+                            <a class="nav-link active" aria-current="page" href="{{url('/')}}">Home</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="<?= url('cursos'); ?>">Cursos</a>
+                            <a class="nav-link active" aria-current="page" href="{{url('cursos')}}">Cursos</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="<?= url('admin/cursos'); ?>">Administrar Cursos</a>
+                            <a class="nav-link active" aria-current="page" href="{{url('admin/cursos')}}">Administrar Cursos</a>
                         </li>
                     </ul>
                 </div>
