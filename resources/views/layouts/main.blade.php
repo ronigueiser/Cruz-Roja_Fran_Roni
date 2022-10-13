9bc2d39573e909d1ad0db5f1dd93bb095ac77ccc
dreo <79935220+4ndreo@users.noreply.github.com>
2022-10-13 18:37:58 -0300

    bootstrap

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -9,10 +9,7 @@
     <title>@yield('title')</title>
 
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
-          rel="stylesheet"
-          integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
-          crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ url('css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{url('css/estilos.css')}}">
 
 </head>
@@ -28,13 +25,13 @@
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="{{url('/')}}">Home</a>
+                        <a class="nav-link" aria-current="page" href="{{url('/')}}">Home</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="{{url('cursos')}}">Cursos</a>
+                        <a class="nav-link" href="{{url('cursos')}}">Cursos</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="{{url('admin/cursos')}}">Administrar Cursos</a>
+                        <a class="nav-link" href="{{url('admin/cursos')}}">Administrar Cursos</a>
                     </li>
                 </ul>
             </div>
