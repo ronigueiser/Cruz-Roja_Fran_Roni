05bd0c93079d7c0b1c7d82c0611885cc3940cf6d
dreo <79935220+4ndreo@users.noreply.github.com>
2022-10-15 15:58:42 -0300

    Update CursosController.php

--- app/Http/Controllers/CursosController.php
+++ app/Http/Controllers/CursosController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Curso;
 
 class CursosController extends Controller
 {
     public function cursos()
     {
-        return view('cursos');
+        $cursos = Curso::all();
+
+        return view('cursos', [
+            'cursos' => $cursos,
+        ]);
     }
 }
