375926507c37eca76ba2b5b40310c8a9760c9f46
ronigueiser <roni.gueiser@davinci.edu.ar>
2022-11-20 20:08:26 -0300

    abm user (leer desc)
    ver y eliminar funcionando, el editar queda modificar el funcionamiento (creo que en el controller, pero todavia no se hacerlo)
    Ajuste como seria para la entrega, en el panel solo se puede "ver", el resto esta comentado porque no lo pide y creo que es insistente en cumplir lo que dice la pauta. (modificado en index.blade de usuarios

--- resources/views/admin/usuarios/eliminar.blade.php
+++ resources/views/admin/usuarios/eliminar.blade.php
@@ -4,12 +4,14 @@
 
 @section('main')
 <section class="container my-4 mb-4">
-    <h1>{{$curso->nombre}}</h1>
-    <p>¿Seguro querés eliminar el curso?</p>
-    <form action="{{route('admin.cursos.eliminar.ejecutar', ['id' =>$curso -> curso_id])}}" method="post">
+    <h1>{{$usuario->email}}</h1>
+    <p>¿Seguro querés eliminar el usuario?</p>
+    <form action="{{route('admin.usuarios.eliminar.ejecutar', ['id' =>$usuario -> usuario_id])}}" method="post">
         @csrf
         <button type="submit" class="btn btn-danger">Eliminar definitivamente</button>
     </form>
+
+    <a class="" aria-current="page" href="{{url('admin/usuarios')}}">Volver </a>
 </section>
 
 @endsection
--- resources/views/admin/usuarios/ver.blade.php
+++ resources/views/admin/usuarios/ver.blade.php
@@ -6,13 +6,23 @@
 @section('main')
         <section class="container d-flex flex-column my-4 mb-4">
             <div class="cont-det-curso">
+
+{{--                <pre>{{$usuario}}</pre>--}}
+
                 <h1 class="negrita">{{$usuario->email}}</h1>
 
-                <p><span class="negrita">Rol:</span> ${{$usuario->role->nombre}}</p>
+                <h2>Curso contratado:</h2>
+                <img class="curso-det-img" src="{{url('img/'.$usuario->curso->portada)}}" alt="{{$usuario->curso->portada_descripcion}}">
 
-                <h2>Curso contratado</h2>
-                <p>{{$usuario->curso->nombre}}</p>
+{{--                <p><span class="negrita">Rol:</span> {{$usuario->role->nombre}}</p>--}}
+
+
+
+                <h3>{{$usuario->curso->nombre}}</h3>
+                <p><span class="negrita">Precio:</span> ${{$usuario->curso->precio}}</p>
+                <p>{{$usuario->curso->descripcion}}</p>
             </div>
+
 
 
             <a class="" aria-current="page" href="{{url('admin/usuarios')}}">Volver </a>
