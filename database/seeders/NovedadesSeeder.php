69d8cf347ba6851e06feaf47db8b91a85e880787
ronigueiser <roni.gueiser@davinci.edu.ar>
2022-11-21 14:10:25 -0300

    Update NovedadesSeeder.php

--- database/seeders/NovedadesSeeder.php
+++ database/seeders/NovedadesSeeder.php
@@ -18,39 +18,27 @@ class NovedadesSeeder extends Seeder
         DB::table('novedades')->insert([
             [
                 'novedad_id'=> 1,
-                'titulo' => 'Actividad 1',
+                'titulo' => 'Charlas Informativas',
                 'curso_id' => 2,
-                'detalle' => 'Detalle de la actividad 1',
+                'detalle' => 'Se asistió a la Escuela Técnica ORT, donde pudimos dar clases de reanimación cardiopulmonar (RCP) a los alumnos de último año. Un ejercicio práctico con su teoría para que los chicos entiendan la importancia de tener estos conocimientos en momentos críticos.',
             ],
             [
                 'novedad_id'=> 2,
-                'titulo' => 'Actividad 2',
-                'curso_id' => 1,
-                'detalle' => 'Detalle de la actividad 1',
+                'titulo' => 'Viaje Grupal a la Costa',
+                'curso_id' => 4,
+                'detalle' => 'Se organizó un viaje de 3 días a la costa argentina para poner en práctica lo aprendido en el curso. Tuvimos la oportunidad de estar presente en las playas de Miramar donde realizamos entrenamientos, prácticas y charlas con los guardavidas de la zona. Una actividad para disfrutar y aprender de la mejor manera.',
             ],
             [
                 'novedad_id'=> 3,
-                'titulo' => 'Actividad 3',
-                'curso_id' => 3,
-                'detalle' => 'Detalle de la actividad 1',
+                'titulo' => 'Experiencias Personales',
+                'curso_id' => 5,
+                'detalle' => 'Tuvimos la oportunidad de hablar con enfermeros voluntariados que viajaron a la zona de ucrania para ayudar a las víctimas de los ataques rusos. Nos cuentan su experiencia trabajando en altas revoluciones, la importancia de atender y dar un primer diagnóstico sobre el estado de salud para poder derivarlo a un hospital o medico capacitado.',
             ],
             [
                 'novedad_id'=> 4,
-                'titulo' => 'Actividad 4',
-                'curso_id' => 2,
-                'detalle' => 'Detalle de la actividad 1',
-            ],
-            [
-                'novedad_id'=> 5,
-                'titulo' => 'Actividad 5',
-                'curso_id' => 5,
-                'detalle' => 'Detalle de la actividad 1',
-            ],
-            [
-                'novedad_id'=> 6,
-                'titulo' => 'Actividad 6',
-                'curso_id' => 2,
-                'detalle' => 'Detalle de la actividad 1',
+                'titulo' => 'Experiencia Laboral',
+                'curso_id' => 3,
+                'detalle' => 'Gracias a nuestra amplia red de contactos, tuvimos la suerte de poder ser parte de una operación en el hospital Trinidad de San Isidro, donde los alumnos de la Tecnicatura superior en instrumentación quirúrgica pudieron ver cómo trabaja un profesional a la hora de preparar una operación.',
             ],
         ]);
     }
